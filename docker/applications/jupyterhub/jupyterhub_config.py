96911dbb265f324d2403da287aa2c90166e48658
bookchaowalit <bookchaowalit@gmail.com>
2025-07-31 16:07:14 +0700

    fix: Standardize healthcheck command formatting across multiple services

--- docker/applications/jupyterhub/jupyterhub_config.py
+++ docker/applications/jupyterhub/jupyterhub_config.py
@@ -5,20 +5,22 @@ import os
 c = get_config()
 
 # Docker spawner configuration
-c.JupyterHub.spawner_class = 'dockerspawner.DockerSpawner'
-c.DockerSpawner.image = os.environ.get('DOCKER_JUPYTER_CONTAINER', 'jupyter/datascience-notebook:latest')
-c.DockerSpawner.network_name = os.environ.get('DOCKER_NETWORK_NAME', 'shared-networks')
+c.JupyterHub.spawner_class = "dockerspawner.DockerSpawner"
+c.DockerSpawner.image = os.environ.get(
+    "DOCKER_JUPYTER_CONTAINER", "jupyter/datascience-notebook:latest"
+)
+c.DockerSpawner.network_name = os.environ.get("DOCKER_NETWORK_NAME", "shared-networks")
 c.DockerSpawner.remove = True
 c.DockerSpawner.debug = True
 
 # Hub configuration
-c.JupyterHub.hub_ip = '0.0.0.0'
+c.JupyterHub.hub_ip = "0.0.0.0"
 c.JupyterHub.hub_port = 8000
 
 # Authentication
-c.Authenticator.admin_users = {'admin'}
+c.Authenticator.admin_users = {"admin"}
 c.LocalAuthenticator.create_system_users = True
 
 # Networking
-c.JupyterHub.ip = '0.0.0.0'
+c.JupyterHub.ip = "0.0.0.0"
 c.JupyterHub.port = 8000
